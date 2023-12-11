7275b9353071d0c3fd4b5a0adf7a52dbfe5724fb
Cody L. Wellman <cody@codexmicro.systems>
2023-12-10 23:51:53 -0500

    Quick re-write & swap turn & drive CAN id order

--- src/main/java/frc/team3602/robot/RobotContainer.java
+++ src/main/java/frc/team3602/robot/RobotContainer.java
@@ -6,27 +6,21 @@
 
 package frc.team3602.robot;
 
-import com.ctre.phoenix6.Utils;
-
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.team3602.robot.subsystems.VisionSubsystem;
-import frc.team3602.robot.subsystems.drivetrain.DrivetrainSubsytem;
+
+import static frc.team3602.robot.Constants.OperatorInterfaceConstants.*;
+import static frc.team3602.robot.Constants.DrivetrainConstants.*;
+import frc.team3602.robot.subsystems.drivetrain.DrivetrainSubsystem;
 
 public class RobotContainer {
   // Subsystems
-  private final DrivetrainSubsytem drivetrainSubsys = Constants.DrivetrainConstants.DRIVETRAIN_SUBSYTEM;
-  private final VisionSubsystem visionSubsys = new VisionSubsystem();
+  private final DrivetrainSubsystem drivetrainSubsys = DRIVETRAIN_SUBSYS;
 
   // Operator interfaces
-  private final CommandXboxController xboxController = new CommandXboxController(
-      Constants.OperatorInterfaceConstants.XBOX_CONTROLLER_PORT);
+  private final CommandXboxController xboxController = new CommandXboxController(XBOX_CONTROLLER_PORT);
 
   // Autonomous
   private final SendableChooser<Command> sendableChooser = new SendableChooser<>();
@@ -35,37 +29,22 @@ public class RobotContainer {
     configDefaultCommands();
     configButtonBindings();
     configAutonomous();
-
-    drivetrainSubsys.registerTelemetry(drivetrainSubsys.telemetryLogger::telemeterize);
   }
 
   private void configDefaultCommands() {
-    drivetrainSubsys.setDefaultCommand(drivetrainSubsys.applyRequest(() -> drivetrainSubsys.fieldCentricDrive
-        .withVelocityX(
-            MathUtil.applyDeadband(xboxController.getLeftY(), 0.02) * Constants.DrivetrainConstants.MAX_SPEED)
-        .withVelocityY(
-            MathUtil.applyDeadband(xboxController.getLeftX(), 0.02) * Constants.DrivetrainConstants.MAX_SPEED)
-        .withRotationalRate(
-            MathUtil.applyDeadband(xboxController.getRightX(), 0.02)
-                * Constants.DrivetrainConstants.MAX_ANGULAR_RATE)));
+    drivetrainSubsys
+        .setDefaultCommand(drivetrainSubsys.applyRequest(
+            () -> drivetrainSubsys.fieldCentricDrive
+                .withVelocityX(-xboxController.getLeftY() * MAX_SPEED)
+                .withVelocityY(-xboxController.getLeftX() * MAX_SPEED)
+                .withRotationalRate(-xboxController.getRightX() * MAX_ANGULAR_RATE)));
   }
 
   private void configButtonBindings() {
-    xboxController.a().whileTrue(drivetrainSubsys.applyRequest(() -> drivetrainSubsys.swerveDriveBrake));
-
-    xboxController.b()
-        .whileTrue(drivetrainSubsys.applyRequest(() -> drivetrainSubsys.pointWheelsAt
-            .withModuleDirection(new Rotation2d(-xboxController.getLeftY(), -xboxController.getLeftX()))));
-
-    if (Utils.isSimulation()) {
-      drivetrainSubsys.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
-    }
   }
 
   private void configAutonomous() {
     SmartDashboard.putData(sendableChooser);
-
-    sendableChooser.addOption("Swerve Controller Command", drivetrainSubsys.swerveControllerCommand());
   }
 
   public Command getAutonomousCommand() {
---
+++ src/main/java/frc/team3602/robot/subsystems/drivetrain/DrivetrainSubsystem.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023, Team 3602. All rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found
+ * in the root directory of this project.
+ */
+
+package frc.team3602.robot.subsystems.drivetrain;
+
+import java.util.function.Supplier;
+
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import static frc.team3602.robot.Constants.DrivetrainConstants.*;
+
+public class DrivetrainSubsystem extends SwerveDrivetrain implements Subsystem {
+  private final SwerveRequest.ApplyChassisSpeeds autonomousRequest = new SwerveRequest.ApplyChassisSpeeds();
+  public final SwerveRequest.FieldCentric fieldCentricDrive = new SwerveRequest.FieldCentric()
+      .withIsOpenLoop(true)
+      .withDeadband(0.02 * MAX_SPEED)
+      .withRotationalDeadband(0.02 * MAX_ANGULAR_RATE);
+
+  private final ChoreoTrajectory trajectory = Choreo.getTrajectory("trajectory");
+
+  public DrivetrainSubsystem(SwerveDrivetrainConstants drivetrainConstants, double odometryUpdateFrequency,
+      SwerveModuleConstants... moduleConstants) {
+    super(drivetrainConstants, odometryUpdateFrequency, moduleConstants);
+  }
+
+  public DrivetrainSubsystem(SwerveDrivetrainConstants drivetrainConstants, SwerveModuleConstants... moduleConstants) {
+    super(drivetrainConstants, moduleConstants);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This is an estimate.
+    updateSimState(0.02, 12);
+  }
+
+  public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+    return run(() -> this.setControl(requestSupplier.get()));
+  }
+
+  public Command swerveControllerCommand() {
+    return Choreo.choreoSwerveCommand(
+        trajectory,
+        () -> this.getState().Pose,
+        new PIDController(0.0, 0.0, 0.0),
+        new PIDController(0.0, 0.0, 0.0),
+        new PIDController(0.0, 0.0, 0.0),
+        (speeds) -> this.setControl(autonomousRequest.withSpeeds(speeds)),
+        true,
+        this);
+  }
+}
